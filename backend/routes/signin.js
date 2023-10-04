a08cb0d45715b89a6c428d05dc81315558b9b23f
Ruslan Lyahov <ruslanlyahov93@yandex.ru>
2023-10-05 02:19:23 +0700

    тест ошибки

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.post('/', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
