fb763b203b9187302e867f93660e9f5481455923
Ruslan Lyahov <ruslanlyahov93@yandex.ru>
2023-10-05 02:00:02 +0700

    добавлена ошибка

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const auth = require('../middlewares/auth');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use('/signup', require('./signup'));
 router.use('/signin', require('./signin'));
 router.use('/cards', auth, require('./cards'));
